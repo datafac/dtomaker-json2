a711ce1fa1211f4b1dfe8c7c7bd44647701597a7
Psiman62 <psiman-git@bytz.me>
2025-11-22 19:29:16 +1000

    more tests

---
+++ DTOMaker.SrcGen.JsonSystemText.IntTests/RoundtripBasicTypeTests_UInt08.cs
@@ -0,0 +1,40 @@
+using DTOMaker.Models;
+using DTOMaker.Runtime;
+using DTOMaker.Runtime.JsonSystemText;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+namespace DTOMaker.SrcGen.JsonSystemText.IntTests;
+
+[Entity][Id(8)]
+public interface ISimpleDTO_UInt08 : IEntityBase
+{
+    [Member(1)] Byte Field1 { get; set; }
+    [Member(2)] Byte? Field2 { get; set; }
+}
+
+public class RoundtripBasicTypeTests_UInt08
+{
+    public string Roundtrip_UInt08(Byte reqValue, Byte? optValue)
+    {
+        var orig = new SimpleDTO_UInt08 { Field1 = reqValue, Field2 = optValue };
+        orig.Freeze();
+        orig.Field1.ShouldBe(reqValue);
+        orig.Field2.ShouldBe(optValue);
+        var json = orig.SerializeToJson();
+        var copy = json.DeserializeFromJson<SimpleDTO_UInt08>();
+        copy.ShouldNotBeNull();
+        copy.ShouldBe(orig);
+        copy.Field1.ShouldBe(reqValue);
+        return json;
+    }
+
+    [Fact] public async Task Roundtrip_UInt08_Defaults() => await Verifier.Verify(Roundtrip_UInt08(default, default));
+    [Fact] public async Task Roundtrip_UInt08_MaxValue() => await Verifier.Verify(Roundtrip_UInt08(Byte.MaxValue, Byte.MaxValue));
+    [Fact] public async Task Roundtrip_UInt08_MinValue() => await Verifier.Verify(Roundtrip_UInt08(Byte.MinValue, Byte.MinValue));
+    [Fact] public async Task Roundtrip_UInt08_UnitVals() => await Verifier.Verify(Roundtrip_UInt08(1, 1));
+
+}
---
+++ DTOMaker.SrcGen.JsonSystemText.IntTests/RoundtripBasicTypeTests_UInt16.cs
@@ -0,0 +1,40 @@
+using DTOMaker.Models;
+using DTOMaker.Runtime;
+using DTOMaker.Runtime.JsonSystemText;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+namespace DTOMaker.SrcGen.JsonSystemText.IntTests;
+
+[Entity][Id(7)]
+public interface ISimpleDTO_UInt16 : IEntityBase
+{
+    [Member(1)] UInt16 Field1 { get; set; }
+    [Member(2)] UInt16? Field2 { get; set; }
+}
+
+public class RoundtripBasicTypeTests_UInt16
+{
+    public string Roundtrip_UInt16(UInt16 reqValue, UInt16? optValue)
+    {
+        var orig = new SimpleDTO_UInt16 { Field1 = reqValue, Field2 = optValue };
+        orig.Freeze();
+        orig.Field1.ShouldBe(reqValue);
+        orig.Field2.ShouldBe(optValue);
+        var json = orig.SerializeToJson();
+        var copy = json.DeserializeFromJson<SimpleDTO_UInt16>();
+        copy.ShouldNotBeNull();
+        copy.ShouldBe(orig);
+        copy.Field1.ShouldBe(reqValue);
+        return json;
+    }
+
+    [Fact] public async Task Roundtrip_UInt16_Defaults() => await Verifier.Verify(Roundtrip_UInt16(default, default));
+    [Fact] public async Task Roundtrip_UInt16_MaxValue() => await Verifier.Verify(Roundtrip_UInt16(UInt16.MaxValue, UInt16.MaxValue));
+    [Fact] public async Task Roundtrip_UInt16_MinValue() => await Verifier.Verify(Roundtrip_UInt16(UInt16.MinValue, UInt16.MinValue));
+    [Fact] public async Task Roundtrip_UInt16_UnitVals() => await Verifier.Verify(Roundtrip_UInt16(1, 1));
+
+}
